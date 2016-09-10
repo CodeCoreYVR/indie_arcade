b354f8b62971e79de90e4b7f22e81148cb48003f
Matt Chang <matthewchang3150@hotmail.com>
2016-09-10 16:45:00 -0700

    Adding reviews feature

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -3,17 +3,12 @@ class ReviewsController < ApplicationController
   def show
     @game = Game.find params[:game_id]
     @reviews = @game.reviews
-    @fun = 0
-    @playability = 0
-    @difficulty = 0
-    @reviews.each do |rev|
-      @fun += rev.fun.to_f
-      @playability += rev.playability.to_f
-      @difficulty += rev.difficulty.to_f
-      @fun /= @reviews.count
-      @playability /= @reviews.count
-      @difficulty /= @reviews.count
-    end
+
+    review_collection = @game.reviews
+    @fun = review_collection.average(:fun).round(3)
+    @playability = review_collection.average(:playability).round(3)
+    @difficulty = review_collection.average(:difficulty).round(3)
+
   end
 
 end
