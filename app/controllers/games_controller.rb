8e88be4e70ce4b3ec03bfe096dcfc695650f3084
Jason Au <jasontau@gmail.com>
2016-09-10 19:40:14 -0700

    Added games create action, added picture and other fields to game

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -19,8 +19,30 @@ class GamesController < ApplicationController
     @rating = (review_collection.average(:fun) + review_collection.average(:playability) + review_collection.average(:difficulty))/3
   end
 
+  def new
+    @game = Game.new
+  end
+
+  def create
+    @game = Game.new(game_params)
+
+    respond_to do |format|
+      if @game.save
+        format.html { redirect_to games_path, notice: 'Game was successfully uploaded.' }
+        format.json { render :index, status: :created, location: @game }
+      else
+        format.html { render :new }
+        format.json { render json: @game.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
   private
   def find_game
     @game = Game.find params[:id]
+  end
+
+  def game_params
+    params.require(:game).permit(:title, :cpu, :gpu, :ram, :size, :approval_date, :current_status, :stats, :description, :picture, :link)
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160910210457) do
+ActiveRecord::Schema.define(version: 20160911020204) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -28,6 +28,9 @@ ActiveRecord::Schema.define(version: 20160910210457) do
     t.datetime "created_at",     null: false
     t.datetime "updated_at",     null: false
     t.integer  "status_id"
+    t.text     "description"
+    t.string   "picture"
+    t.string   "link"
     t.index ["status_id"], name: "index_games_on_status_id", using: :btree
     t.index ["user_id"], name: "index_games_on_user_id", using: :btree
   end
