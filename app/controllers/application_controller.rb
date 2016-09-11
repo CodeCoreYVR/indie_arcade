33ef2eaca579b2136ae03d77352249c7f7e87d19
mokbeili <mo.kbeili@gmail.com>
2016-09-11 10:46:54 -0700

    Added search by tag functionality

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,33 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  def user_signed_in?
+  session[:user_id].present?
+  end
+  helper_method :user_signed_in?
+# adding a helper_method call as in above, allows the view files (all of them
+# in this case) to have access to this method
+
+  def current_user
+  # the technique below is called memoization which fetched the user in this
+  # case the first time you call the method and every subsequent time it uses
+  # the one stored in the `@current_user` variable
+    if user_signed_in?
+      @current_user ||= User.find session[:user_id]
+    end
+  end
+
+  def user_admin?
+  # the technique below is called memoization which fetched the user in this
+  # case the first time you call the method and every subsequent time it uses
+  # the one stored in the `@current_user` variable
+    user_signed_in? && current_user.admin?
+  end
+
+helper_method :current_user
+
+  def authenticate_user!
+    redirect_to new_session_path unless user_signed_in?
+  end
+
 end
--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,10 +1,17 @@
 class GamesController < ApplicationController
   def index
+    # byebug
     @tags = Tag.all
-    if params[:search]
-      @games = User.search(params[:search]).includes(:games).map(&:games).flatten
+    @tagsearch = Tag.ids.map{|x| x.to_s}
+    byebug
+    if params[:search_user]
+      @games = User.search(params[:search_user]).includes(:games).map(&:games).flatten
+      byebug
+    elsif (@tagsearch & params.keys).empty?
+      @games = Game.all
     else
-    @games = Game.all
+      @games = Tag.where(id: (@tagsearch & params.keys)).map{|k| k.games}.flatten
+      @games.uniq
     end
   end
 
