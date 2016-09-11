667b216b267d8b795eb8314d67c3b3ac4917fa2a
Matt Chang <matthewchang3150@hotmail.com>
2016-09-11 09:57:29 -0700

    Changes to hidden search bar

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -16,9 +16,22 @@
 //= require chosen-jquery
 //= require bootstrap-sprockets
 //= require_tree .
-$(document).ready(function(){
-    $("#home-slideshow").carousel({
-        interval : 2000,
-        pause: false
+  $(document).ready(function(){
+      $("#home-slideshow").carousel({
+          interval : 2000,
+          pause: false
+      });
+
+    $(window).keypress(function(event) {
+        if (event.which == 13) {
+            event.preventDefault();
+            $("form").submit();
+        }
+        console.log(event.which);
     });
-})
+
+    $('#search').click(function(){
+      $('#search-wrapper').toggleClass('moved');
+    })
+
+  })
