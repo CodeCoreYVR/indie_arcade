10e2f6514a89d4c520d2cdce5ef507a702cfe7fb
Matt Chang <matthewchang3150@hotmail.com>
2016-09-10 19:00:43 -0700

    Fixed signup bugs

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,10 @@ class UsersController < ApplicationController
   before_action :set_user, only: [:show, :edit, :update, :destroy]
 
 
+  def index
+    redirect_to root_
+  end
+
   # GET /users/1
   # GET /users/1.json
   # def show
@@ -20,10 +24,10 @@ class UsersController < ApplicationController
   # POST /users.json
   def create
     @user = User.new(user_params)
-
     respond_to do |format|
       if @user.save
-        format.html { redirect_to users_path, notice: 'User was successfully created.' }
+        session[:user_id] = @user.id
+        format.html { redirect_to root_path, notice: 'User was successfully created.' }
         format.json { render :index, status: :created, location: @user }
       else
         format.html { render :new }
