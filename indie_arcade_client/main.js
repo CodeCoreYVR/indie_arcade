51c47ea1249667196c9234b9b50430fb45cb8b40
Jason Au <jasontau@gmail.com>
2016-09-11 18:10:16 -0700

    Stashing changes

--- indie_arcade_client/main.js
+++ indie_arcade_client/main.js
@@ -1,9 +1,25 @@
 $(document).ready( function() {
+  var currentGames;
+
   $.get("http:/localhost:3000/games.json", function(data){
-    for (i in data) {
-      var template = $("#game-listing-tmpl").html();
-      var renderedHTML = Mustache.render(template, data[i]);
-      $('#games').append( renderedHTML );
-    }
+    currentGames = data;
+    console.log(data);
+    // for (i in data) {
+    //   var template = $("#game-listing-tmpl").html();
+    //   var renderedHTML = Mustache.render(template, data[i]);
+    //   $('#games').append( renderedHTML );
+    // }
   });
+
+  $("#play-game").click(function() {
+    console.log("Clicked Play");
+    var random = Math.floor(Math.random() * (currentGames.length - 1 + 1)) + 1;
+
+    var template = $("#game-listing-tmpl").html();
+    var renderedHTML = Mustache.render(template, currentGames[random]);
+    $('#games').children().remove();
+    $('#games').append( renderedHTML );
+  });
+
+
 });
