f1b4bf8d5ae7edf7dd12d04b76a2285b1c8c60eb
Vic Wang <jerrywangvic@gmail.com>
2016-09-11 17:51:19 -0700

    uncommented game model validations

--- app/models/game.rb
+++ app/models/game.rb
@@ -14,19 +14,19 @@ class Game < ApplicationRecord
   MAXIMUM_RAM = 4000
   MAXIMUM_HD_SPACE = 6000
 
-  # validates :title, presence: true,
-  #                   uniqueness: {case_sensitive: false}
-  # validates :cpu, presence: true,
-  #                 numericality: {less_than_or_equal_to: MAXIMUM_CPU}
-  # validates :gpu, presence: true,
-  #                 numericality: {less_than_or_equal_to: MAXIMUM_GPU}
-  # validates :ram, presence: true,
-  #                 numericality: {less_than_or_equal_to: MAXIMUM_RAM}
-  # validates :size, presence: true,
-  #                 numericality: {less_than_or_equal_to: MAXIMUM_HD_SPACE}
-  # validates :user_id, presence: true
-  # validates :status_id, presence: true
-  # validates :description, presence: true
+  validates :title, presence: true,
+                    uniqueness: {case_sensitive: false}
+  validates :cpu, presence: true,
+                  numericality: {less_than_or_equal_to: MAXIMUM_CPU}
+  validates :gpu, presence: true,
+                  numericality: {less_than_or_equal_to: MAXIMUM_GPU}
+  validates :ram, presence: true,
+                  numericality: {less_than_or_equal_to: MAXIMUM_RAM}
+  validates :size, presence: true,
+                  numericality: {less_than_or_equal_to: MAXIMUM_HD_SPACE}
+  validates :user_id, presence: true
+  validates :status_id, presence: true
+  validates :description, presence: true
 
   mount_uploader :picture, PictureUploader
 
