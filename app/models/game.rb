23a2119ea62e99483e6e42bd5d0aa4627bda7936
Jason Au <jasontau@gmail.com>
2016-09-21 11:10:01 -0700

    refactored game show controllers and game show rspec

--- app/models/game.rb
+++ app/models/game.rb
@@ -38,4 +38,11 @@ class Game < ApplicationRecord
     Game.where("status_id = ? OR status_id = ?", "1", "2" )
   end
 
+  # Usage example: @game.average_score_for :fun
+  def average_score_for( attribute )
+    reviews.average( attribute ).round(2) * 20
+  end
+
+
+
 end
--- spec/controllers/games_controller_spec.rb
+++ spec/controllers/games_controller_spec.rb
@@ -1,25 +1,36 @@
 require 'rails_helper'
 
 RSpec.describe GamesController, type: :controller do
+  let(:user) { User.create password:"123456", email:"fake@fake.com", company:"Fake Co" }
+  let(:admin) { User.create password:"123456564123", email:"admin_fake@fake.com", company:"Fake Co Admin" }
+  let(:game) { Game.create title:"Fake Game 43895738", user: user, description:"This is a fake game description" }
+
   describe "#show" do
-    def create_valid_user
-      User.create password:"123456", email:"fake@fake.com", company:"Fake Co"
-    end
-
-    def create_valid_game
-      Game.create title:"Fake Game 43895738", user:create_valid_user, description:"This is a fake game description"
-    end
-
-    it "renders the show template" do
-      get :show, params: { id: create_valid_game.id }
-      expect(response).to render_template(:show)
-    end
 
     it "defines an instance variable for game based on passed id" do
-      game = create_valid_game
-
       get :show, params: { id: game.id }
       expect(assigns(:game)).to eq(game)
     end
+
+    it "renders the show template" do
+      get :show, params: { id: game }
+      expect(response).to render_template(:show)
+    end
+
+    context "with signed in user" do
+      it "is an admin" do
+        session[:user_id] = admin.id
+        get :show, params: { id: game }
+        expect(response).to render_template(:show)
+      end
+
+      it "is a developer" do
+        session[:user_id] = user.id
+        get :show, params: { id: game }
+        expect(response).to render_template(:show)
+      end
+    end
+
+
   end
 end
