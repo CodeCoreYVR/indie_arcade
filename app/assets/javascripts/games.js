b37b8d40dacca8f50af257fbe0ae918c2042cd8d
keplerf <keplerweb@gmail.com>
2016-09-23 10:17:40 -0700

    styling search page

--- app/assets/javascripts/games.js
+++ app/assets/javascripts/games.js
@@ -1,15 +1,22 @@
 $(document).ready(function(){
 // COLOR STYLING OF GAME STATUS'
+   var panel = $('.container').find('.panel-result');
+   panel.each(function(i){
+     setTimeout(function(){
+       $('.panel-result').eq(i).addClass('is-visible');
+     }, 200 * i);
+   });
 
-  $('.game-status').each(function(){
-    if ($(this).html() == "Game under review") {
-      $(this).css({'color': 'blue'});
-    }
-    else if ($(this).html() == "Game not uploaded") {
-      $(this).css({'color': 'red'});
-    }
-    else if ($(this).html() == "Rejected") {
-      $(this).css({'color': 'red'});
-    }
-  });
+
+  // $('.game-status').each(function(){
+  //   if ($(this).html() == "Game under review") {
+  //     $(this).css({'color': 'blue'});
+  //   }
+  //   else if ($(this).html() == "Game not uploaded") {
+  //     $(this).css({'color': 'red'});
+  //   }
+  //   else if ($(this).html() == "Rejected") {
+  //     $(this).css({'color': 'red'});
+  //   }
+  // });
 });
