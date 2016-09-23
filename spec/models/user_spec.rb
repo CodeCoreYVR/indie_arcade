243607605b549b4c93f54a761130e44ab8cc50a8
Ela Tan <ela.tan@gmail.com>
2016-09-23 11:57:27 -0700

    Done with user model and user model validations

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,40 +3,61 @@ require 'rails_helper'
 RSpec.describe User, type: :model do
 
   describe "validations" do
-    it "requires a valid email address" do
-      user = User.new
-      user.valid?
-      expect(user.errors).to(have_key(:email))
+    context "on a new user" do
+      it "requires a valid email address" do
+        user = User.new
+        user.valid?
+        expect(user.errors).to(have_key(:email))
+      end
+
+      it "requires a company presence" do
+        user = User.new
+        user.valid?
+        expect(user.errors).to(have_key(:company))
+      end
+
+      it "should not be valid without a password" do
+        user = User.new password: nil, password_confirmation: nil
+        expect(user).not_to be_valid
+      end
+
+      it "should not be valid with a short password " do
+        user = User.new password: 'short', password_confirmation: 'short'
+        expect(user).not_to be_valid
+      end
+
+      it "should not be valid with a confirmation mismatch " do
+        user = User.new password: 'short', password_confirmation: 'long'
+        expect(user).not_to be_valid
+      end
     end
 
-    it "requires a company presence" do
-      user = User.new
-      user.valid?
-      expect(message.errors).to(have_key(:company))
-    end
+    context "on an existing user" do
+      let(:user) do
+        u = User.create company: 'mcdonald', email: 'mcdonald@gmail.com', password: 'password', password_confirmation: 'password'
+        User.find u.id
+      end
 
-    it "requires a company presence" do
-      user = User.new
-      user.valid?
-      expect(message.errors).to(have_key(:company))
-    end
+      it "should be valid with no changes" do
+        expect(user).to be_valid
+      end
 
-    it "requires a company presence" do
-      user = User.new
-      user.valid?
-      expect(message.errors).to(have_key(:company))
-    end
+      it "should not be valid with same company" do
+        b = User.create company: 'mcdonald', email: 'mcdonald@gmail.com', password: 'password', password_confirmation: 'password'
+        b = User.create company: 'mcdonald', email: 'mcdonaldfastfood@gmail.com', password: 'password1', password_confirmation: 'password1'
+        expect(b).not_to be_valid
+      end
 
-    it "requires a password presence" do
-      user = User.new
-      user.valid?
-      expect(message.errors).to(have_key(:password))
-    end
+      it "should not be valid with same email" do
+        b = User.create company: 'mcdonald', email: 'mcdonald@gmail.com', password: 'password', password_confirmation: 'password'
+        b = User.create company: 'mcdonaldfastfood', email: 'mcdonald@gmail.com', password: 'password1', password_confirmation: 'password1'
+        expect(b).not_to be_valid
+      end
 
-    it "requires a password presence" do
-      user = User.new
-      user.valid?
-      expect(message.errors).to(have_key(:password))
+      it "should be valid with new matching password" do
+        b = User.create company: 'mcdonaldfastfood', email: 'mcdonald@gmail.com', password: 'september', password_confirmation: 'september'
+        expect(b).to be_valid
+      end
     end
 
 
