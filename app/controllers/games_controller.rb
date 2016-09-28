0e0b43592dd00c984973976e733e1c7e692d7998
Tim Eijgelshoven <tgeijgelshoven@gmail.com>
2016-09-28 09:59:11 -0700

    Commenced refactoring games controller to pass Rubocop

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -8,22 +8,23 @@ class GamesController < ApplicationController
     @tags = Tag.all
 
     if params[:search_user]
-      @games = Game.user_data_subset(user_is_admin?,user_is_dev?,
-      current_user.nil? ? nil : current_user.id).search_by('user',
-      params[:search_user]).order(desired_order)
+      @games = Game.user_data_subset(
+        user_is_admin?, user_is_dev?, current_user&.id
+      ).search_by('user', params[:search_user]).order(desired_order)
     elsif params[:search_main]
-      @games = Game.user_data_subset(user_is_admin?,user_is_dev?,
-      current_user.nil? ? nil : current_user.id).search_by('main',
-      params[:search_main]).order(desired_order)
+      @games = Game.user_data_subset(
+        user_is_admin?, user_is_dev?, current_user&.id
+      ).search_by('main', params[:search_main]).order(desired_order)
     elsif params[:tag]
-      @games = Game.user_data_subset(user_is_admin?,user_is_dev?,
-      current_user.nil? ? nil : current_user.id).search_by('tags',
-      params.require(:tag)[:tag_ids]).order(desired_order)
+      @games = Game.user_data_subset(
+        user_is_admin?, user_is_dev?, current_user&.id
+      ).search_by('tags', params.require(:tag)[:tag_ids]).order(desired_order)
     else
-      @games = Game.user_data_subset(user_is_admin?,user_is_dev?,
-      current_user.nil? ? nil : current_user.id).order(desired_order)
+      @games = Game.user_data_subset(
+        user_is_admin?, user_is_dev?, current_user&.id
+      ).order(desired_order)
     end
-      @games = @games.page(params[:page]).per(GAMES_PER_PAGE)
+    @games = @games.page(params[:page]).per(GAMES_PER_PAGE)
   end
 
   def show
@@ -36,11 +37,20 @@ class GamesController < ApplicationController
     else
       respond_to do |format|
         if game.update(game_params)
-          format.html { redirect_to @game, notice: 'Game status was successfully updated.' }
-          format.json { render :show, status: :ok, location: @game }
+          format.html do
+            redirect_to @game,
+                        notice: 'Game status was successfully updated.'
+          end
+          format.json do
+            render :show,
+                   status: :ok, location: @game
+          end
         else
           format.html { render :edit }
-          format.json { render json: @game.errors, status: :unprocessable_entity }
+          format.json do
+            render json: @game.errors,
+                   status: :unprocessable_entity
+          end
         end
       end
     end
@@ -56,7 +66,10 @@ class GamesController < ApplicationController
 
     respond_to do |format|
       if @game.save
-        format.html { redirect_to games_path, notice: 'Game was successfully uploaded.' }
+        format.html do
+          redirect_to games_path,
+                      notice: 'Game was successfully uploaded.'
+        end
         format.json { render :index, status: :created, location: @game }
       else
         format.html { render :new }
@@ -67,9 +80,7 @@ class GamesController < ApplicationController
 
   def edit
     game = Game.find params[:id]
-    if cannot? :manage, game
-      redirect_to root_path
-    end
+    redirect_to root_path if cannot? :manage, game
   end
 
   def destroy
@@ -82,7 +93,6 @@ class GamesController < ApplicationController
     end
   end
 
-
   private
 
   def find_game
@@ -90,14 +100,15 @@ class GamesController < ApplicationController
   end
 
   def game_params
-    params.require(:game).permit(:title, :cpu, :gpu, :ram, :size, :approval_date,
-                                  :status_id, :stats, :description, :picture,
-                                  :attachment, :link, {tag_ids: [] } )
+    params.require(:game).permit(:title, :cpu, :gpu, :ram, :size,
+                                 :approval_date, :status_id, :stats,
+                                 :description, :picture, :attachment,
+                                 :link, tag_ids: [])
   end
 
   # Used to fulfill client requests for 5 new games
   def fill_machine_order(games)
-    games.limit(5).order("RANDOM()")
+    games.limit(5).order('RANDOM()')
   end
 
   def desired_order
