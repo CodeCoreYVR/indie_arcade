6063b14d0ce0423dd5f134093bd53f93b2b2c3a7
Tim Eijgelshoven <tgeijgelshoven@gmail.com>
2016-09-28 17:10:00 -0700

    Remove commented out code from games factory

--- spec/factories/games.rb
+++ spec/factories/games.rb
@@ -10,24 +10,3 @@ FactoryGirl.define do
     aasm_state "Released to arcade"
   end
 end
-
-# t.string   "title"
-# t.float    "cpu"
-# t.float    "gpu"
-# t.float    "ram"
-# t.float    "size"
-# t.datetime "approval_date"
-# t.string   "current_status"
-# t.text     "stats"
-# t.integer  "user_id"
-# t.datetime "created_at",     null: false
-# t.datetime "updated_at",     null: false
-# t.integer  "status_id"
-# t.text     "description"
-# t.string   "picture"
-# t.string   "link"
-# t.string   "attachment"
-# t.string   "aasm_state"
-# t.json     "key_map"
-# t.index ["status_id"], name: "index_games_on_status_id", using: :btree
-# t.index ["user_id"], name: "index_games_on_user_id", using: :btree
