f444f03980f8ef0b64129b30ac2a66e3e0b1ec9d
mokbeili <mo.kbeili@gmail.com>
2016-10-27 20:14:14 -0600

    Removed search related test as we no longer use method 'search'

--- spec/models/game_spec.rb
+++ spec/models/game_spec.rb
@@ -7,12 +7,4 @@ RSpec.describe Game, type: :model do
     new_game.title = older_game.title
     expect(new_game).not_to be_valid
   end
-
-  it 'returns a list of titles' do
-    FactoryGirl.create(:game, title: 'bobGames')
-    FactoryGirl.create(:game, title: 'newBob')
-    FactoryGirl.create(:game, title: 'john')
-    outcome = Game.search('bob')
-    expect(outcome.map(&:title)).to eq(%w(bobGames newBob))
-  end
 end
