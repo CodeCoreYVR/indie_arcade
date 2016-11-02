31fa72e06fad504c297ee89a7df87b3edcef67a1
Tammam Kbeili <tammam.kbeili@gmail.com>
2016-11-02 15:58:47 -0700

    use environment var for AWS bucket

--- config/initializers/fog_setting.rb
+++ config/initializers/fog_setting.rb
@@ -5,7 +5,7 @@
        aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'] || '',
        region:                'us-west-2'
      }
-     config.fog_directory  = 'awesomesanswer22e'
+     config.fog_directory  = ENV['AWS_BUCKET']
      config.fog_public     = false                                        # optional, defaults to true
      config.fog_attributes = { 'Cache-Control' => "max-age=#{365.day.to_i}" } # optional, defaults to {}
    end
