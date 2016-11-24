4d18ab20418229142de93aa89f5821b2aff74f49
Calvin McConnell <calvin.mcconnell20@gmail.com>
2016-11-24 09:56:07 -0800

    add user show page and link to dashboard

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,9 +1,11 @@
 module ApplicationHelper
-  def display_picture(game)
-    if game.picture.nil?
-      image_tag('app/assets/images/fallback/default.png')
-    else
-      image_tag(game.picture.url)
-    end
-  end
+ def display_picture(game)
+   if game.nil?
+     image_tag('/assets/fallback/default-feecc56fc0572402530a9509051a194266f25d1887ff725d395306367ed34c2f.png')
+   elsif game.picture.nil?
+     image_tag('app/assets/images/fallback/default.png')
+   else
+     image_tag(game.picture.url)
+   end
+ end
 end
