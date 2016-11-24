b1159754a9042390779bf1f88e0ad73479373d35
cgsimmons <chris.simmons.alt@gmail.com>
2016-11-24 13:10:31 -0800

    Change layouts

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,4 +6,12 @@ module ApplicationHelper
       image_tag(game.picture.url)
     end
   end
+
+  # def background_path(game)
+  #   if game.picture.nil?
+  #     image_url('fallback/default.png')
+  #   else
+  #     game.picture.url
+  #   end
+  # end
 end
