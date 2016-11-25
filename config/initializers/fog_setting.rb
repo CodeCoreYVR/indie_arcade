dd565a9feed37ad9705d4a952ddb69e02d523946
Song Ji <songji0509@gmail.com>
2016-11-25 10:27:49 -0800

    Fix fog-setting.rb indentation

--- config/initializers/fog_setting.rb
+++ config/initializers/fog_setting.rb
@@ -1,13 +1,13 @@
  CarrierWave.configure do |config|
-    config.fog_provider = 'fog/aws'
-    config.fog_credentials = {
-       provider:              'AWS',
-       aws_access_key_id:     ENV['AWS_ACCESS_KEY_ID'] || '',
-       aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'] || '',
-       region:                'us-west-2'
-     }
-     # Change folder name to s3 bucket name in production env
-     config.fog_directory  = 'awesomesanswer22e'
-     config.fog_public     = false                                        # optional, defaults to true
-     config.fog_attributes = { 'Cache-Control' => "max-age=#{365.day.to_i}" } # optional, defaults to {}
-   end
+  config.fog_provider = 'fog/aws'
+  config.fog_credentials = {
+     provider:              'AWS',
+     aws_access_key_id:     ENV['AWS_ACCESS_KEY_ID'] || '',
+     aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'] || '',
+     region:                'us-west-2'
+   }
+   # Change folder name to s3 bucket name in production env
+   config.fog_directory  = 'awesomesanswer22e'
+   config.fog_public     = false                                        # optional, defaults to true
+   config.fog_attributes = { 'Cache-Control' => "max-age=#{365.day.to_i}" } # optional, defaults to {}
+end
