f4ff7a07f8348a7813361f4dbf09e7ee71610b91
Chris Dimoff <chrisdimoff@gmail.com>
2016-12-01 09:24:21 -0800

    Changed storage location for development to file. Fixed other syntax issues recommended in pull request.

--- app/uploaders/logo_uploader.rb
+++ app/uploaders/logo_uploader.rb
@@ -1,7 +1,7 @@
 class LogoUploader < CarrierWave::Uploader::Base
   include CarrierWave::MiniMagick
-  storage :fog
-  # storage :file
+
+
   def store_dir
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
--- config/initializers/fog_setting.rb
+++ config/initializers/fog_setting.rb
@@ -1,19 +1,19 @@
  CarrierWave.configure do |config|
-    # if Rails.env.development?
-    #   config.storage = :file
-    #   config.enable_processing = false
-    # else
-    # config.storage = :fog
-    config.fog_provider = 'fog/aws'
-    config.fog_credentials = {
-       provider:              'AWS',
-       aws_access_key_id:     ENV['AWS_ACCESS_KEY_ID'] || '',
-       aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'] || '',
-       region:                'us-west-2'
-     }
-     # Change folder name to s3 bucket name in production env
-     config.fog_directory  = 'awesomesanswer22e'
-     config.fog_public     = false                                        # optional, defaults to true
-     config.fog_attributes = { 'Cache-Control' => "max-age=#{365.day.to_i}" } # optional, defaults to {}
+    if Rails.env.development?
+        config.storage = :file
+    else
+      config.storage = :fog
+      config.fog_provider = 'fog/aws'
+      config.fog_credentials = {
+         provider:              'AWS',
+         aws_access_key_id:     ENV['AWS_ACCESS_KEY_ID'] || '',
+         aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'] || '',
+         region:                'us-west-2'
+       }
+       # Change folder name to s3 bucket name in production env
+       config.fog_directory  = 'awesomesanswer22e'
+       config.fog_public     = false                                        # optional, defaults to true
+       config.fog_attributes = { 'Cache-Control' => "max-age=#{365.day.to_i}" } # optional, defaults to {}
+      end
    end
   # end
